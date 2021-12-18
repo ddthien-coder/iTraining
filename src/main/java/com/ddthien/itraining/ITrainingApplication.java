cfe54550ce1185cfff56486f85bdea19eb10c8c5
ddthien-coder <jungjihoonkr97@gmail.com>
2021-12-18 15:37:54 +0700

    init

---
+++ src/main/java/com/ddthien/itraining/ITrainingApplication.java
@@ -0,0 +1,13 @@
+package com.ddthien.itraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ITrainingApplication.class, args);
+    }
+
+}
